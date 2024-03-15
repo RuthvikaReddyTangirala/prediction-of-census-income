2ebc300425f291340a0aa752ac5a6caf002996e9
RuthvikaReddyTangirala <ruthvikareddytangirala20@gmail.com>
2024-03-14 22:52:11 -0400

    modified knn.py and logistic_regression.py

--- src/knn.py
+++ src/knn.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import StandardScaler
-from sklearn.model_selection import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, ConfusionMatrixDisplay, roc_curve, roc_auc_score, precision_score, recall_score, f1_score
 
 df = pd.read_csv('data\preprocessed_data.csv')
 
@@ -11,7 +12,21 @@ y= df['income']
 #splitting the train and test data
 X_train, X_test, y_train, y_test = train_test_split(X, y , test_size=0.3, random_state= 0)
 
+#Feature scaling for standardisation
 scaler = StandardScaler()
 X_train_Scaled = scaler.fit_transform(X_train)
 X_test_Scaled = scaler.transform(X_test) 
+
+#Initialising the model
+k = 5
+model = KNeighborsClassifier(n_neighbors=k)
+model.fit(X_train_Scaled, y_train)
+
+#Performing predictions on the test set
+predictions = model.predict(X_test_Scaled) 
+
+# Evaluate the model
+print("Accuracy:", accuracy_score(y_test, predictions))
+print("Confusion Matrix:\n", confusion_matrix(y_test, predictions))
+print("Classification Report:\n", classification_report(y_test, predictions))
 
--- src/logistic_regression.py
+++ src/logistic_regression.py
@@ -24,10 +24,12 @@ model.fit(X_train_Scaled, y_train)
 #Performing predictions on the test set
 predictions = model.predict(X_test_Scaled)
 
+#confusion matrix
 Confusion_Matrix = confusion_matrix(y_test, predictions)
 plot_cm = ConfusionMatrixDisplay(confusion_matrix = Confusion_Matrix)
 plot_cm.plot()
 
+#Plotting ROC Curve
 probability_scores = model.predict_proba(X_test_Scaled)[:, 1]
 specificity, sensitivity, thresholds = roc_curve(y_test, probability_scores)
 auc_score = roc_auc_score(y_test, probability_scores)
@@ -43,6 +45,11 @@ plt.title('Receiver Operating Characteristic')
 plt.legend(loc="lower right")
 plt.show() 
 
+
+#Classification Report
+print("Classification Report:\n", classification_report(y_test, predictions))
+
+#Metrics
 results = {
     'Model': ['Logistic Regression'],
     'Accuracy': [accuracy_score(y_test, predictions)],
